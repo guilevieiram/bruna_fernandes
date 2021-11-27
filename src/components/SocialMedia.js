22b6f09679c50c2abffc1c3dad2337633c69e811
guilherme-vieira <guilhermevmanhaes@gmail.com>
2021-11-27 17:51:22 +0100

    fixed link social media bug

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -6,12 +6,12 @@ function MediaLink({image, hoverName, link}){
     const [hover, setHover] = useState(false)
     const toggleHover = () => setHover(!hover)
     return(
-        <div key={link} onMouseEnter={toggleHover} onMouseLeave={toggleHover} href={link}  className="h-6 w-6 m-4 transform transition-all hover:-translate-y-1 flex items-center">
+        <a key={link} onMouseEnter={toggleHover} onMouseLeave={toggleHover} href={link}  className="h-6 w-6 m-4 transform transition-all hover:-translate-y-1 flex items-center">
             <img src={image} alt={hoverName}  title={hoverName} />
             <div className={`px-2 w-max text-sm transform transition-all ${hover ? " translate-x-0 opacity-100" : "opacity-0 scale-0"} hover:opacity-100 hover:translate-x-0 hover:underline `}>
                 <p className="backdrop-filter backdrop-blur-sm bg-light bg-opacity-30 shadow-lg w-max px-4 py-1 rounded-md">{hoverName}</p>
             </div>
-        </div>
+        </a>
     )
 }
 
